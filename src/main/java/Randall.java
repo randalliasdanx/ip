1b52b75c07e1bccd0839897af299dc9dd40dad47
Randall Koh <randallkoh2003@gmail.com>
2026-01-18 12:17:28 +0800

    Create Randall.java for Level 1

---
+++ src/main/java/Randall.java
@@ -0,0 +1,10 @@
+public class Randy {
+    public static void main(String[] args) {
+        System.out.println("_________________________________________________________");
+        System.out.println("Hello! I'm Randy");
+        System.out.println("What can I do for you?");
+        System.out.println("_________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_________________________________________________________");
+    }
+}
