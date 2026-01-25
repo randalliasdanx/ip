2dc0dc64e9057ae9ac71089bcf32a1fb3e8bd826
Randall Koh <randallkoh2003@gmail.com>
2026-01-25 15:32:12 +0800

    Level 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,36 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
+public class Deadline extends Task {
+    protected Object by; // Store the deadline as LocalDate or String
+
+    // Constructor accepting a description and a date string
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            this.by = LocalDate.parse(by);  // Parse the date string to LocalDate
+        } catch (DateTimeParseException e) {
+            this.by = by;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        // Format the deadline in "MMM dd yyyy" format
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        if (by instanceof LocalDate) {
+            return "[D]" + super.toString() + " (by: " + ((LocalDate) by).format(formatter) + ")";
+        } else {
+            return "[D]" + super.toString() + " (by: " + by + ")";
+        }
+    }
+
+    @Override
+    public boolean occursOn(LocalDate date) {
+        if (by instanceof LocalDate) {
+            return ((LocalDate) by).equals(date);
+        }
+        return false;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,49 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
-    protected String start;
-    protected String end;
+    protected Object start; // Can be LocalDate or String
+    protected Object end;   // Can be LocalDate or String
 
     public Event(String description, String start, String end) {
         super(description);
-        this.start = start;
-        this.end = end;
+        // Try to parse start date
+        try {
+            this.start = LocalDate.parse(start);
+        } catch (DateTimeParseException e) {
+            this.start = start;
+        }
+        // Try to parse end date
+        try {
+            this.end = LocalDate.parse(end);
+        } catch (DateTimeParseException e) {
+            this.end = end;
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        String startStr = (start instanceof LocalDate) 
+            ? ((LocalDate) start).format(formatter) 
+            : start.toString();
+        String endStr = (end instanceof LocalDate) 
+            ? ((LocalDate) end).format(formatter) 
+            : end.toString();
+        return "[E]" + super.toString() + " (from: " + startStr + " to: " + endStr + ")";
+    }
+
+    @Override
+    public boolean occursOn(LocalDate date) {
+        // Check if date falls within the event range (inclusive)
+        if (start instanceof LocalDate && end instanceof LocalDate) {
+            LocalDate startDate = (LocalDate) start;
+            LocalDate endDate = (LocalDate) end;
+            return !date.isBefore(startDate) && !date.isAfter(endDate);
+        } else if (start instanceof LocalDate) {
+            return ((LocalDate) start).equals(date);
+        }
+        return false;
     }
 }
