df1855b39d9485959c7c9d8c72b8b9939ef0dd8f
Randall Koh <randallkoh2003@gmail.com>
2026-01-30 20:53:26 +0800

    Code Event class which is a subclass of Task

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,9 +2,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+// This is the Event Class which extends Task
 public class Event extends Task {
     protected Object start; // Can be LocalDate or String
-    protected Object end;   // Can be LocalDate or String
+    protected Object end; // Can be LocalDate or String
 
     public Event(String description, String start, String end) {
         super(description);
@@ -25,12 +26,12 @@ public class Event extends Task {
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
-        String startStr = (start instanceof LocalDate) 
-            ? ((LocalDate) start).format(formatter) 
-            : start.toString();
-        String endStr = (end instanceof LocalDate) 
-            ? ((LocalDate) end).format(formatter) 
-            : end.toString();
+        String startStr = (start instanceof LocalDate)
+                ? ((LocalDate) start).format(formatter)
+                : start.toString();
+        String endStr = (end instanceof LocalDate)
+                ? ((LocalDate) end).format(formatter)
+                : end.toString();
         return "[E]" + super.toString() + " (from: " + startStr + " to: " + endStr + ")";
     }
 
