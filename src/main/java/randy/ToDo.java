bf15eadf4b7d1fa518f79dd0c6a5ca0f0998e11b
Randall Koh <randallkoh2003@gmail.com>
2026-02-14 17:45:24 +0800

    Improve ToDo class documentation
    Add comprehensive Javadoc to the ToDo class explaining
    its purpose as the simplest task type. Document constructor
    and toString method for better code maintainability.

--- src/main/java/randy/ToDo.java
+++ src/main/java/randy/ToDo.java
@@ -1,13 +1,23 @@
 package randy;
 
 /**
- * Basic task with no date attached.
+ * Represents a simple todo task without any date or time constraints.
+ * This is the most basic type of task in the system.
  */
 public class ToDo extends Task {
+
+    /**
+     * Creates a new ToDo task.
+     *
+     * @param desc Description of what needs to be done
+     */
     public ToDo(String desc) {
         super(desc);
     }
 
+    /**
+     * Returns string representation with [T] prefix.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
